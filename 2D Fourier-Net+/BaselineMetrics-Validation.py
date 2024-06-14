8da1c4737c02346f48a099bcdcc8d7139a9a6051
Jan Meyer <jmeyer@rechenknecht03.imi.uni-luebeck.de>
2024-06-14 14:13:42 +0200

    Changed baseline validation metric calculation to be specific for subset mode

--- 2D Fourier-Net+/BaselineMetrics-Validation.py
+++ 2D Fourier-Net+/BaselineMetrics-Validation.py
@@ -15,45 +15,21 @@ from skimage.metrics import structural_similarity, mean_squared_error
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 
 parser = ArgumentParser()
-parser.add_argument("--lr", type=float,
-                    dest="lr", default=1e-4, help="learning rate")
 parser.add_argument("--bs", type=int,
                     dest="bs", default=1, help="batch_size")
-parser.add_argument("--iteration", type=int,
-                    dest="iteration", default=320001,
-                    help="number of total iterations")
-parser.add_argument("--smth_lambda", type=float,
-                    dest="smth_lambda", default=0.02,
-                    help="lambda loss: suggested range 0.1 to 10")
-parser.add_argument("--checkpoint", type=int,
-                    dest="checkpoint", default=1, 
-                    help="frequency of saving models")
-parser.add_argument("--start_channel", type=int,
-                    dest="start_channel", default=8,
-                    help="number of start channels")
 parser.add_argument("--datapath", type=str,
                     dest="datapath",
                     default='/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon',
                     #default='/home/jmeyer/storage/datasets/CMRxRecon/MultiCoil/Cine/TrainingSet/FullSample', #AccFactor04
                     help="data path for training images")
-parser.add_argument("--choose_loss", type=int,
-                    dest="choose_loss",
-                    default=1,
-                    help="choose similarity loss: SAD (0), MSE (1), NCC (2), SSIM (3)")
 parser.add_argument("--mode", type=int,
                     dest="mode",
                     default='0',
                     help="choose dataset mode: fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3)")
 opt = parser.parse_args()
 
-lr = opt.lr
 bs = opt.bs
-iteration = opt.iteration
-start_channel = opt.start_channel
-n_checkpoint = opt.checkpoint
-smooth = opt.smth_lambda
 datapath = opt.datapath
-choose_loss = opt.choose_loss
 mode = opt.mode
 
 use_cuda = True
@@ -63,7 +39,7 @@ device = torch.device("cuda" if use_cuda else "cpu")
 validation_set = ValidationDatasetCMR(datapath, mode) 
 validation_generator = Data.DataLoader(dataset=validation_set, batch_size=bs, shuffle=True, num_workers=4)
 
-csv_name = './BaselineMetrics-Validation.csv'
+csv_name = './BaselineMetrics-Validation-Mode' + str(mode) + '.csv'
 f = open(csv_name, 'w')
 with f:
     fnames = ['Index','MSE','SSIM','Mean MSE','Mean SSIM']
