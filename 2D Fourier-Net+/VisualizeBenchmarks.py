2bb9961cee8b588c7aa4197c2f909d9b3fdf4be5
Jan Meyer <jmeyer@rechenknecht03.imi.uni-luebeck.de>
2024-06-17 13:07:15 +0200

    Added training results for Acc10 data and expanded VisualizeBenchmarks.py to incorporate these

--- 2D Fourier-Net+/VisualizeBenchmarks.py
+++ 2D Fourier-Net+/VisualizeBenchmarks.py
@@ -10,6 +10,10 @@ filename2 = foldername2 + '_Png/' + foldername2 + '.csv'
 foldername3 = 'Loss_1_Chan_8_Smth_0.01_LR_0.0001_Mode_2'
 filename3 = foldername3 + '_Png/' + foldername3 + '.csv'
 
+foldername4 = 'Loss_1_Chan_8_Smth_0.01_LR_0.0001_Mode_3'
+filename4 = foldername4 + '_Png/' + foldername4 + '.csv'
+
+
 # for debugging
 #path = './2D Fourier-Net+/'
 #filename1 = path + foldername1 + '_Png/' + foldername1 + '.csv'
@@ -49,6 +53,18 @@ with open(filename3,'r') as csvfile:
             index3.append(int(row[0])) 
             MSE3.append(float(row[1])) 
             SSIM3.append(float(row[2]))
+            
+index4 = [] 
+MSE4 = [] 
+SSIM4 = []
+
+with open(filename4,'r') as csvfile: 
+    lines = csv.reader(csvfile, delimiter=',') 
+    for i, row in enumerate(lines): 
+        if i>0:
+            index4.append(int(row[0])) 
+            MSE4.append(float(row[1])) 
+            SSIM4.append(float(row[2]))
 
 plt.subplots(figsize=(10, 4))  
 plt.title('Validation Benchmark Results', fontsize = 20) 
@@ -59,6 +75,7 @@ plt.subplot(1,2,1)
 plt.plot(index1, MSE1, color = 'g', label = "Fully Sampled") 
 plt.plot(index2, MSE2, color = 'r', label = "Subsampled (Acc4)") 
 plt.plot(index3, MSE3, color = 'b', label = "Subsampled (Acc8)") 
+plt.plot(index4, MSE4, color = 'y', label = "Subsampled (Acc10)") 
 plt.xlabel('Iterations')
 plt.ylabel('MSE') 
 plt.legend()
@@ -68,9 +85,10 @@ plt.subplot(1,2,2)
 plt.plot(index1, SSIM1, color = 'g', label = "Fully Sampled") 
 plt.plot(index2, SSIM2, color = 'r', label = "Subsampled (Acc4)") 
 plt.plot(index3, SSIM3, color = 'b', label = "Subsampled (Acc8)") 
+plt.plot(index4, SSIM4, color = 'y', label = "Subsampled (Acc10)") 
 plt.xlabel('Iterations') 
 plt.ylabel('SSIM') 
 plt.legend()
 
-plt.savefig('/home/jmeyer/storage/students/janmeyer_711878/Master-Arbeit/Thesis/Images/ValidationBenchmark-' + foldername1 + '+Mode1+Mode2.png')
+plt.savefig('/home/jmeyer/storage/students/janmeyer_711878/Master-Arbeit/Thesis/Images/ValidationBenchmark-' + foldername1 + '+Mode1+Mode2+Mode3.png')
 plt.close()
