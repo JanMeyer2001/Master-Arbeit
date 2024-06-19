0a61eb6d778021b3737785740ab5d4363e16eaae
Jan Meyer <jmeyer@rechenknecht03.imi.uni-luebeck.de>
2024-06-19 15:10:42 +0200

    Added mode as a parser argument to choose between fully sampled and subsampled data. Also now changes paths accordingly.

--- 2D_CMR_Registration/NiftiConverter.py
+++ 2D_CMR_Registration/NiftiConverter.py
@@ -1,4 +1,4 @@
-from os import makedirs, mkdir
+from os import mkdir
 from os.path import isdir, join
 from argparse import ArgumentParser
 import numpy as np
@@ -7,7 +7,6 @@ from Models import *
 from Functions import *
 import torch.utils.data as Data
 import nibabel
-from skimage.metrics import structural_similarity, mean_squared_error
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -17,35 +16,53 @@ parser.add_argument("--datapath", type=str,
                     default='/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon',
                     #default='/home/jmeyer/storage/datasets/CMRxRecon/MultiCoil/Cine/TrainingSet/FullSample', #AccFactor04
                     help="data path for training images")
+parser.add_argument("--mode", type=int,
+                    dest="mode",
+                    default='0',
+                    help="choose dataset mode: fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3)")
 opt = parser.parse_args()
+
 datapath = opt.datapath
+mode = opt.mode
 
 bs = 1
 use_cuda = True
 device = torch.device("cuda" if use_cuda else "cpu")
-test_set = TestDatasetCMRBenchmark(data_path=datapath, mode=0)
+test_set = TestDatasetCMRBenchmark(data_path=datapath, mode=mode)
 test_generator = Data.DataLoader(dataset=test_set, batch_size=bs, shuffle=False, num_workers=2)
-save_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti'
+
+base_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti'
+if mode == 0:
+    save_path = join(base_path, 'Nifti_FullySampled')
+elif mode == 1:
+    save_path = join(base_path, 'Nifti_Acc4') 
+elif mode == 2:
+    save_path = join(base_path, 'Nifti_Acc8') 
+elif mode == 3:
+    save_path = join(base_path, 'Nifti_Acc10')   
+else:
+    save_path = None
+    print('Wrong mode argument!! Must choose from either fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3).')     
+
 num_image_pair = 1
 
-for mov_img, fix_img, _, _ in test_generator: 
-    with torch.no_grad():
-        # convert to numpy arrays
-        mov_img = mov_img[0,0,:,:].cpu().numpy()
-        fix_img = fix_img[0,0,:,:].cpu().numpy()
-        
-        # convert to Nifit format
-        mov_img_nifti = nibabel.Nifti1Image(mov_img, np.eye(4))
-        fix_img_nifti = nibabel.Nifti1Image(fix_img, np.eye(4))
-        
-        # create folder if it does not exist 
-        path = join(save_path,'ImagePair'+str(num_image_pair))
-        if not isdir(path):
-            mkdir(path)
+for _, _, mov_img, fix_img in test_generator: 
+    # convert to numpy arrays
+    mov_img = mov_img[0,0,:,:].cpu().numpy()
+    fix_img = fix_img[0,0,:,:].cpu().numpy()
+    
+    # convert to Nifit format
+    mov_img_nifti = nibabel.Nifti1Image(mov_img, np.eye(4))
+    fix_img_nifti = nibabel.Nifti1Image(fix_img, np.eye(4))
+    
+    # create folder if it does not exist 
+    path = join(save_path,'ImagePair'+str(num_image_pair))
+    if not isdir(path):
+        mkdir(path)
 
-        # save Nifti images
-        nibabel.save(mov_img_nifti, join(path,'MovingImage'))
-        nibabel.save(fix_img_nifti, join(path,'FixedImage'))
+    # save Nifti images
+    nibabel.save(mov_img_nifti, join(path,'MovingImage'))
+    nibabel.save(fix_img_nifti, join(path,'FixedImage'))
 
-        # increment counter for image pair number
-        num_image_pair += 1
+    # increment counter for image pair number
+    num_image_pair += 1
