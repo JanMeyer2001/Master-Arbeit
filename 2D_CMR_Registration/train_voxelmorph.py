c087f472c9cb4c2ba85606177dc6166812a97a3b
Jan Meyer <jmeyer@rechenknecht03.imi.uni-luebeck.de>
2024-07-04 12:13:29 +0200

    Made inshape specific for every dataset and changed levels and features to work for ACDC data

--- 2D_CMR_Registration/train_voxelmorph.py
+++ 2D_CMR_Registration/train_voxelmorph.py
@@ -16,7 +16,7 @@ from os.path import isdir, join
 parser = ArgumentParser()
 parser.add_argument("--learning_rate", type=float,
                     dest="learning_rate", default=1e-4, help="learning rate")
-parser.add_argument("--epochs", type=int, dest="epochs", default=100,
+parser.add_argument("--epochs", type=int, dest="epochs", default=50,
                     help="number of total epochs")
 parser.add_argument("--lambda", type=float, dest="smth_lambda", default=0.01,
                     help="lambda loss: suggested range 0.1 to 10")
@@ -26,8 +26,9 @@ parser.add_argument("--dataset", type=str,
                     help="dataset for training images: Select either ACDC, CMRxRecon or OASIS")
 parser.add_argument("--mode", type=int, dest="mode", default=0,
                     help="choose dataset mode: fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3)")
-parser.add_argument('--image_loss', type=str, dest="image_loss", default='mse',
-                    help='image reconstruction loss - can be mse or ncc (default: mse)')
+parser.add_argument("--choose_loss", type=int,
+                    dest="choose_loss", default=1,
+                    help="choose similarity loss: MSE (0) or NCC (1)")
 parser.add_argument("--earlyStop", type=bool,
                     dest="earlyStop", default=True, action=BooleanOptionalAction, 
                     help="choose whether to use early stopping to prevent overfitting (True) or not (False)")
@@ -39,7 +40,7 @@ epochs = opt.epochs
 smth_lambda = opt.smth_lambda
 dataset = opt.dataset
 mode = opt.mode
-image_loss = opt.image_loss
+choose_loss = opt.choose_loss
 earlyStop = opt.earlyStop
 
 # load CMR training, validation and test data
@@ -50,24 +51,27 @@ if dataset == 'ACDC':
     training_generator = Data.DataLoader(dataset=train_set, batch_size=1, shuffle=True, num_workers=4)
     validation_set = ValidationDatasetACDC('/home/jmeyer/storage/students/janmeyer_711878/data/ACDC', mode) 
     validation_generator = Data.DataLoader(dataset=validation_set, batch_size=1, shuffle=True, num_workers=4)
+    input_shape = [216,256]
 elif dataset == 'CMRxRecon':
     # load CMRxRecon data
     train_set = TrainDatasetCMRxRecon('/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon', mode) 
     training_generator = Data.DataLoader(dataset=train_set, batch_size=1, shuffle=True, num_workers=4)
     validation_set = ValidationDatasetCMRxRecon('/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon', mode) 
     validation_generator = Data.DataLoader(dataset=validation_set, batch_size=1, shuffle=True, num_workers=4)
+    input_shape = [82,170]
 elif dataset == 'OASIS':
     # path for OASIS dataset
     train_set = TrainDatasetOASIS('/imagedata/Learn2Reg_Dataset_release_v1.1/OASIS',trainingset = 4) 
     training_generator = Data.DataLoader(dataset=train_set, batch_size=1, shuffle=True, num_workers=4)
-    valid_set = ValidationDatasetOASIS('/imagedata/Learn2Reg_Dataset_release_v1.1/OASIS')
-    valid_generator = Data.DataLoader(dataset=valid_set, batch_size=1, shuffle=False, num_workers=2)
+    validation_set = ValidationDatasetOASIS('/imagedata/Learn2Reg_Dataset_release_v1.1/OASIS')
+    validation_generator = Data.DataLoader(dataset=validation_set, batch_size=1, shuffle=False, num_workers=2)
+    input_shape = [160,224]
 else:
     print('Incorrect dataset selected!! Must be either ACDC, CMRxRecon or OASIS...')
 
 # path to save model parameters to
-model_dir = './ModelParameters-{}/Voxelmorph_Loss_{}_Smth_{}_LR_{}_Mode_{}/'.format(dataset,image_loss,smth_lambda,learning_rate,mode)
-model_dir_png = './ModelParameters-{}/Voxelmorph_Png/'.format(dataset)
+model_dir = './ModelParameters-{}/Voxelmorph_Loss_{}_Smth_{}_LR_{}_Mode_{}/'.format(dataset,choose_loss,smth_lambda,learning_rate,mode)
+model_dir_png = './ModelParameters-{}/Voxelmorph_Loss_{}_Smth_{}_LR_{}_Mode_{}_Png/'.format(dataset,choose_loss,smth_lambda,learning_rate,mode)
 
 if not isdir(model_dir):
     mkdir(model_dir)
@@ -75,7 +79,7 @@ if not isdir(model_dir):
 if not isdir(model_dir_png):
     mkdir(model_dir_png)
 
-csv_name = model_dir_png + 'Voxelmorph_Loss_{}_Smth_{}_LR_{}_Mode_{}.csv'.format(image_loss,smth_lambda,learning_rate,mode)
+csv_name = model_dir_png + 'Voxelmorph_Loss_{}_Smth_{}_LR_{}_Mode_{}.csv'.format(choose_loss,smth_lambda,learning_rate,mode)
 f = open(csv_name, 'w')
 with f:
     if dataset == 'CMRxRecon':
@@ -85,24 +89,20 @@ with f:
     writer = csv.DictWriter(f, fieldnames=fnames)
     writer.writeheader()
 
-# define model
-enc_nf = [16, 32, 32, 32]
-dec_nf = [32, 32, 32, 32, 32, 16, 16]
-
 # use dense voxelmorph
-model = VxmDense(inshape=[82,170], nb_unet_features=[enc_nf, dec_nf], bidir=False).cuda()     #, int_steps=7, int_downsize=2
+model = VxmDense(inshape=input_shape, nb_unet_features=32, bidir=False, nb_unet_levels=4).cuda()  #, int_steps=7, int_downsize=2
 model.train()  
 
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 transform = SpatialTransform().cuda()
 
 # prepare image loss
-if image_loss == 'ncc':
-    loss_similarity = NCC().loss
-elif image_loss == 'mse':
+if choose_loss == 1:
+    loss_similarity = NCC(win=9)
+elif choose_loss == 0:
     loss_similarity = MSE().loss
 else:
-    raise ValueError('Image loss should be "mse" or "ncc", but found "%s"' % image_loss)
+    raise ValueError('Image loss should be 0 for MSE or 1 for NCC, but found %d' % choose_loss)
 loss_smooth = smoothloss
 
 ##############
@@ -164,8 +164,10 @@ for epoch in range(epochs):
             # calculate MSE, SSIM and Dice 
             MSE_Validation.append(mean_squared_error(warped_mov_img[0,0,:,:].cpu().numpy(), fix_img[0,0,:,:].cpu().numpy()))
             SSIM_Validation.append(structural_similarity(warped_mov_img[0,0,:,:].cpu().numpy(), fix_img[0,0,:,:].cpu().numpy(), data_range=1))
-            if dataset != 'CMRxRecon':
+            if dataset == 'OASIS':
                 Dice_Validation.append(dice(warped_mov_seg[0,0,:,:].cpu().numpy(),fix_seg[0,0,:,:].cpu().numpy()))
+            elif dataset == 'ACDC':
+                Dice_Validation.append(dice_ACDC(warped_mov_seg[0,0,:,:].cpu().numpy(),fix_seg[0,0,:,:].cpu().numpy()))
     
         # calculate mean of validation metrics
         Mean_MSE = np.mean(MSE_Validation)
@@ -211,7 +213,7 @@ for epoch in range(epochs):
         else:
             print("epoch {:d}/{:d} - DICE_val: {:.5f}, SSIM_val: {:.5f}, MSE_val: {:.6f}".format(epoch, epochs, Mean_Dice, Mean_SSIM, Mean_MSE))
 
-        # stop training if metrics stop improving for three epochs (only on the first run)
+        # stop training if metrics stop improving for three epochs 
         if counter_earlyStopping == 3:
             break
             
