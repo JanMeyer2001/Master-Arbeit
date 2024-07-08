8276b228d66dfaf723487d294be11f8108d4ea46
Jan Meyer <jmeyer@rechenknecht03.imi.uni-luebeck.de>
2024-07-08 14:48:36 +0200

    Added option for different datasets to the test evaluation and Nifti converter

--- 2D_CMR_Registration/NiftiConverter.py
+++ 2D_CMR_Registration/NiftiConverter.py
@@ -11,27 +11,40 @@ import warnings
 warnings.filterwarnings("ignore")
 
 parser = ArgumentParser()
-parser.add_argument("--datapath", type=str,
-                    dest="datapath",
-                    default='/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon',
-                    #default='/home/jmeyer/storage/datasets/CMRxRecon/MultiCoil/Cine/TrainingSet/FullSample', #AccFactor04
-                    help="data path for training images")
+parser.add_argument("--dataset", type=str, 
+                    dest="dataset", default="ACDC",
+                    help="dataset for training images: Select either ACDC, CMRxRecon or OASIS")
 parser.add_argument("--mode", type=int,
                     dest="mode",
                     default='0',
                     help="choose dataset mode: fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3)")
 opt = parser.parse_args()
 
-datapath = opt.datapath
+dataset = opt.dataset
 mode = opt.mode
 
 bs = 1
 use_cuda = True
 device = torch.device("cuda" if use_cuda else "cpu")
-test_set = TestDatasetCMRBenchmark(data_path=datapath, mode=mode)
-test_generator = Data.DataLoader(dataset=test_set, batch_size=bs, shuffle=False, num_workers=2)
+if dataset == 'ACDC':
+    # load ACDC test data
+    test_set = TestDatasetACDC('/home/jmeyer/storage/students/janmeyer_711878/data/ACDC', mode) 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+    base_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti/ACDC'
+elif dataset == 'CMRxRecon':
+    # load CMRxRecon test data
+    test_set = TestDatasetCMRxReconBenchmark('/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon', mode) 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+    base_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti/CMRxRecon'
+elif dataset == 'OASIS':
+    # path for OASIS test dataset
+    test_set = TestDatasetOASIS('/imagedata/Learn2Reg_Dataset_release_v1.1/OASIS') 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+    base_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti/OASIS'
+else:
+    raise ValueError('Dataset should be "ACDC", "CMRxRecon" or "OASIS", but found "%s"!' % dataset)
 
-base_path = '/home/jmeyer/storage/students/janmeyer_711878/data/Nifti'
+assert mode >= 0 and mode <= 3, f"Expected mode to be one of fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3), but got: {mode}"
 if mode == 0:
     save_path = join(base_path, 'Nifti_FullySampled')
 elif mode == 1:
@@ -40,29 +53,27 @@ elif mode == 2:
     save_path = join(base_path, 'Nifti_Acc8') 
 elif mode == 3:
     save_path = join(base_path, 'Nifti_Acc10')   
-else:
-    save_path = None
-    print('Wrong mode argument!! Must choose from either fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3).')     
 
-num_image_pair = 1
+if not isdir(save_path):
+    mkdir(save_path)
 
-for _, _, mov_img, fix_img in test_generator: 
-    # convert to numpy arrays
-    mov_img = mov_img[0,0,:,:].cpu().numpy()
-    fix_img = fix_img[0,0,:,:].cpu().numpy()
+for i, image_pair in enumerate(test_generator): 
+    if dataset == 'CMRxRecon':
+        mov_img = image_pair[2][0,0,:,:].cpu().numpy()
+        fix_img = image_pair[3][0,0,:,:].cpu().numpy()
+    else:
+        mov_img = image_pair[0][0,0,:,:].cpu().numpy()
+        fix_img = image_pair[1][0,0,:,:].cpu().numpy()    
     
     # convert to Nifit format
     mov_img_nifti = nibabel.Nifti1Image(mov_img, np.eye(4))
     fix_img_nifti = nibabel.Nifti1Image(fix_img, np.eye(4))
     
     # create folder if it does not exist 
-    path = join(save_path,'ImagePair'+str(num_image_pair))
+    path = join(save_path,'ImagePair{:04d}'.format(i+1))
     if not isdir(path):
         mkdir(path)
 
     # save Nifti images
     nibabel.save(mov_img_nifti, join(path,'MovingImage'))
     nibabel.save(fix_img_nifti, join(path,'FixedImage'))
-
-    # increment counter for image pair number
-    num_image_pair += 1
--- 2D_CMR_Registration/TestBenchmark.py
+++ 2D_CMR_Registration/TestBenchmark.py
@@ -13,23 +13,20 @@ warnings.filterwarnings("ignore")
 import argparse
 
 parser = ArgumentParser()
-parser.add_argument("--lr", type=float,
-                    dest="lr", default=1e-4, help="learning rate")
-parser.add_argument("--smth_lambda", type=float,
+parser.add_argument("--learning_rate", type=float,
+                    dest="learning_rate", default=1e-4, help="learning rate")
+parser.add_argument("--lambda", type=float,
                     dest="smth_lambda", default=0.01,
                     help="lambda loss: suggested range 0.1 to 10")
 parser.add_argument("--start_channel", type=int,
                     dest="start_channel", default=8,
                     help="number of start channels")
-parser.add_argument("--datapath", type=str,
-                    dest="datapath",
-                    default='/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon',
-                    help="data path for training images")
-parser.add_argument("--choose_loss", type=int,
-                    dest="choose_loss", default=1,
+parser.add_argument("--dataset", type=str, 
+                    dest="dataset", default="ACDC",
+                    help="dataset for training images: Select either ACDC, CMRxRecon or OASIS")
+parser.add_argument("--choose_loss", type=int, dest="choose_loss", default=1,
                     help="choose similarity loss: SAD (0), MSE (1), NCC (2), SSIM (3)")
-parser.add_argument("--mode", type=int,
-                    dest="mode", default='0',
+parser.add_argument("--mode", type=int, dest="mode", default='0',
                     help="choose dataset mode: fully sampled (0), 4x accelerated (1), 8x accelerated (2) or 10x accelerated (3)")
 parser.add_argument("--F_Net_plus", type=bool,
                     dest="F_Net_plus", default=True, action=argparse.BooleanOptionalAction, 
@@ -37,21 +34,26 @@ parser.add_argument("--F_Net_plus", type=bool,
 parser.add_argument("--diffeo", type=bool,
                     dest="diffeo", default=True, action=argparse.BooleanOptionalAction, 
                     help="choose whether to use a diffeomorphic transform (True) or not (False)")
+parser.add_argument("--FT_size", type=tuple,
+                    dest="FT_size", default=[24,24],
+                    help="choose size of FT crop: Should be smaller than [40,84].")
 opt = parser.parse_args()
 
-lr = opt.lr
+learning_rate = opt.learning_rate
 start_channel = opt.start_channel
 smooth = opt.smth_lambda
-datapath = opt.datapath
+dataset = opt.dataset
 choose_loss = opt.choose_loss
 mode = opt.mode
 F_Net_plus = opt.F_Net_plus
 diffeo = opt.diffeo
+FT_size = opt.FT_size
 
 # choose the model
 model_name = 0
 if F_Net_plus:
-    model = Cascade(2, 2, start_channel).cuda()
+    assert  FT_size[0] > 0 and FT_size[0] <= 40 and FT_size[1] > 0 and FT_size[1] <= 84, f"Expected FT size smaller or equal to [40, 84] and larger than [0, 0], but got: [{FT_size[0]}, {FT_size[1]}]"
+    model = Cascade(2, 2, start_channel, FT_size).cuda() 
     model_name = 1
 else:     
     model = Fourier_Net(2, 2, start_channel).cuda()  
@@ -64,7 +66,7 @@ if diffeo:
 
 transform = SpatialTransform().cuda()
 
-path = './ModelParameters/Model_{}_Diffeo_{}_Loss_{}_Chan_{}_Smth_{}_LR_{}_Mode_{}_Pth/'.format(model_name,diffeo_name,choose_loss,start_channel,smooth, lr, mode)
+path = './ModelParameters-{}/Model_{}_Diffeo_{}_Loss_{}_Chan_{}_FT_{}-{}_Smth_{}_LR_{}_Mode_{}_Pth/'.format(dataset,model_name,diffeo_name,choose_loss,start_channel,FT_size[0],FT_size[1],smooth,learning_rate,mode)
 model_idx = -1
 from natsort import natsorted
 print('Best model: {}'.format(natsorted(os.listdir(path))[model_idx]))
@@ -75,46 +77,81 @@ torch.backends.cudnn.benchmark = True
 model.load_state_dict(torch.load(modelpath))
 model.eval()
 transform.eval()
-#Dices=[]
+Dice_test = []
 MSE_test = []
 SSIM_test = []
-NegJ=[]
+NegJ = []
 use_cuda = True
 device = torch.device("cuda" if use_cuda else "cpu")
-test_set = TestDatasetCMRxReconBenchmark(data_path=datapath, mode=mode)
-test_generator = Data.DataLoader(dataset=test_set, batch_size=bs, shuffle=False, num_workers=2)
 times = []
 
-csv_name = './TestResults-Metrics/TestMetrics-Model_{}_Diffeo_{}_Loss_{}_Chan_{}_Smth_{}_LR_{}_Mode_{}.csv'.format(model_name,diffeo_name,choose_loss,start_channel,smooth, lr, mode)
+if dataset == 'ACDC':
+    # load ACDC test data
+    test_set = TestDatasetACDC('/home/jmeyer/storage/students/janmeyer_711878/data/ACDC', mode) 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+elif dataset == 'CMRxRecon':
+    # load CMRxRecon test data
+    test_set = TestDatasetCMRxRecon('/home/jmeyer/storage/students/janmeyer_711878/data/CMRxRecon', mode) 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+elif dataset == 'OASIS':
+    # path for OASIS test dataset
+    test_set = TestDatasetOASIS('/imagedata/Learn2Reg_Dataset_release_v1.1/OASIS') 
+    test_generator = Data.DataLoader(dataset=test_set, batch_size=1, shuffle=False, num_workers=4)
+else:
+    raise ValueError('Dataset should be "ACDC", "CMRxRecon" or "OASIS", but found "%s"!' % dataset)
+
+csv_name = './TestResults-{}/TestMetrics-Model_{}_Diffeo_{}_Loss_{}_Chan_{}_FT_{}-{}_Smth_{}_LR_{}_Mode_{}_Pth/'.format(dataset,model_name,diffeo_name,choose_loss,start_channel,FT_size[0],FT_size[1],smooth,learning_rate,mode)
 f = open(csv_name, 'w')
 with f:
-    fnames = ['Image Pair','MSE','SSIM','Time','Mean MSE','Mean SSIM','Mean Time','Mean NegJ']
+    if dataset == 'CMRxRecon':
+        fnames = ['Image Pair','SSIM','MSE','Mean SSIM','Mean MSE','Mean Time','Mean NegJ']
+    else:
+        fnames = ['Image Pair','Dice','SSIM','MSE','Mean SSIM','Mean MSE','Mean Time','Mean NegJ']
     writer = csv.DictWriter(f, fieldnames=fnames)
     writer.writeheader()
 
-image_num = 1 
-for mov_img_fullySampled, fix_img_fullySampled, mov_img_subSampled, fix_img_subSampled in test_generator: #, mov_lab, fix_lab
+for i, image_pairs in enumerate(test_generator): 
     with torch.no_grad():
+        mov_img_fullySampled = image_pairs[0]
+        fix_img_fullySampled = image_pairs[1]
+        if dataset == 'CMRxRecon':
+            mov_img_subSampled = image_pairs[2]
+            fix_img_subSampled = image_pairs[3]
+        else:
+            mov_seg = image_pairs[2]
+            fix_seg = image_pairs[3]
+
         start = time.time()
         # calculate displacement on subsampled data
-        V_xy = model(mov_img_subSampled.float().to(device), fix_img_subSampled.float().to(device))
-        # but warp fully sampled data
-        __, warped_mov_img_fullySampled = transform(mov_img_fullySampled.float().to(device), V_xy.permute(0, 2, 3, 1), mod = 'nearest')
+        if dataset == 'CMRxRecon':
+            V_xy = model(mov_img_subSampled.float().to(device), fix_img_subSampled.float().to(device))
+        else:
+            V_xy = model(mov_img_fullySampled.float().to(device), fix_img_fullySampled.float().to(device))
         
-        #__, warped_mov_lab = transform(mov_lab.float().to(device), V_xy.permute(0, 2, 3, 1), mod = 'nearest') # old code for dice score
-    
         # get inference time
         inference_time = time.time()-start
         times.append(inference_time)
-
-        # convert to numpy
-        warped_mov_img_fullySampled = warped_mov_img_fullySampled[0,0,:,:].cpu().numpy()
-        fix_img_fullySampled = fix_img_fullySampled[0,0,:,:].cpu().numpy()
         
-        # calculate metrics on fully sampled images
-        MSE_test.append(mean_squared_error(warped_mov_img_fullySampled, fix_img_fullySampled))
-        SSIM_test.append(structural_similarity(warped_mov_img_fullySampled, fix_img_fullySampled, data_range=1))
-
+        # but warp fully sampled data
+        __, warped_mov_img_fullySampled = transform(mov_img_fullySampled.float().to(device), V_xy.permute(0, 2, 3, 1), mod = 'nearest')
+        if dataset != 'CMRxRecon':
+            __, warped_mov_seg = transform(mov_seg.float().to(device), V_xy.permute(0, 2, 3, 1), mod = 'nearest')
+        
+        # calculate MSE, SSIM and Dice 
+        if dataset == 'OASIS':
+            csv_Dice = dice(warped_mov_seg[0,0,:,:].cpu().numpy(),fix_seg[0,0,:,:].cpu().numpy())
+        elif dataset == 'ACDC':
+            csv_Dice = dice_ACDC(warped_mov_seg[0,0,:,:].cpu().numpy(),fix_seg[0,0,:,:].cpu().numpy())
+        csv_MSE = mean_squared_error(warped_mov_img_fullySampled[0,0,:,:].cpu().numpy(), fix_img_fullySampled[0,0,:,:].cpu().numpy())
+        csv_SSIM = structural_similarity(warped_mov_img_fullySampled[0,0,:,:].cpu().numpy(), fix_img_fullySampled[0,0,:,:].cpu().numpy(), data_range=1)
+                  
+        MSE_test.append(csv_MSE)
+        SSIM_test.append(csv_SSIM)
+        if dataset == 'OASIS':
+            Dice_test.append(csv_Dice)
+        elif dataset == 'ACDC':
+            Dice_test.append(csv_Dice)
+    
         hh, ww = V_xy.shape[-2:]
         V_xy = V_xy.detach().cpu().numpy()
         V_xy[:,0,:,:] = V_xy[:,0,:,:] * hh / 2
@@ -124,18 +161,14 @@ for mov_img_fullySampled, fix_img_fullySampled, mov_img_subSampled, fix_img_subS
         negJ = np.sum(jac_det <= 0) / 160 / 192 * 100
         NegJ.append(negJ)
 
-        # save to csv file
+        # save test results to csv file
         f = open(csv_name, 'a')
         with f:
             writer = csv.writer(f)
-            writer.writerow([image_num, mean_squared_error(warped_mov_img_fullySampled, fix_img_fullySampled), structural_similarity(warped_mov_img_fullySampled, fix_img_fullySampled, data_range=1), inference_time, '-', '-', '-', '-']) 
-        image_num += 1
-
-        """
-        for bs_index in range(bs):
-            dice_bs = dice(warped_mov_lab[bs_index,...].data.cpu().numpy().copy(),fix_lab[bs_index,...].data.cpu().numpy().copy())
-            Dices.append(dice_bs)
-        """ 
+            if dataset == 'CMRxRecon':
+                writer.writerow([i, csv_MSE, csv_SSIM, inference_time, '-', '-', '-', '-']) 
+            else:
+                writer.writerow([i, csv_Dice, csv_MSE, csv_SSIM, inference_time, '-', '-', '-', '-', '-']) 
 
 mean_MSE = np.mean(MSE_test)
 std_MSE = np.std(MSE_test)
@@ -148,14 +181,20 @@ std_NegJ = np.std(NegJ)
 
 mean_time = np.mean(times)
 
+if dataset != 'CMRxRecon':
+    mean_Dice = np.mean(Dice_test)
+    std_Dice = np.std(Dice_test)
+
 f = open(csv_name, 'a')
 with f:
     writer = csv.writer(f)
-    writer.writerow(['-', '-', '-', '-', mean_MSE, mean_SSIM, mean_time, mean_NegJ])  
+    if dataset == 'CMRxRecon':
+        writer.writerow(['-', '-', '-', mean_MSE, mean_SSIM, mean_time, mean_NegJ])
+    else:
+        writer.writerow(['-', '-', '-', '-', mean_Dice, mean_MSE, mean_SSIM, mean_time, mean_NegJ])
 
-print('Mean inference time: ', mean_time, ' seconds')
-print('Mean MSE: ', mean_MSE, 'Std MSE: ', std_MSE)
-print('Mean SSIM: ', mean_SSIM, 'Std SSIM: ', std_SSIM)
-print('Mean DetJ<0 %:', mean_NegJ, 'Std DetJ<0 %:', std_NegJ)
-#print('Mean Dice Score: ', np.mean(Dices), 'Std Dice Score: ', np.std(Dices))
-
+if dataset == 'CMRxRecon':
+    print('Mean inference time: {:.4f} seconds\n     MSE: {:.6f} +- {:.6f}\n     SSIM: {:.5f} +- {:.5f}\n     DetJ<0 %: {:.4f} +- {:.4f}'.format(mean_time, mean_MSE, std_MSE, mean_SSIM, std_SSIM, mean_NegJ, std_NegJ))
+else:
+    print('Mean inference time: {:.4f} seconds\n     DICE: {:.5f} +- {:.5f}\n     MSE: {:.6f} +- {:.6f}\n     SSIM: {:.5f} +- {:.5f}\n     DetJ<0 %: {:.4f} +- {:.4f}'.format(mean_time, mean_Dice, std_Dice, mean_MSE, std_MSE, mean_SSIM, std_SSIM, mean_NegJ, std_NegJ))
+    
